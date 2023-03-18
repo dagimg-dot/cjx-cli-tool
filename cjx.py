569a51598a85ea659a4cd29805ca9bfe1bd5d72b
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-03-19 00:26:05 +0300

    upto speed with main branch

--- cjx.py
+++ cjx.py
@@ -2,12 +2,10 @@ import argparse
 import json
 import os
 import subprocess
-import winreg
 
 
 class CJX:    
     def __init__(self):
-            self.current_dir = os.getcwd()
             self.parser = argparse.ArgumentParser(prog='cjx')
             self.subparsers = self.parser.add_subparsers(dest='command')
             self.create_parser = self.subparsers.add_parser('create', help='Create a new JavaFX project')
@@ -17,8 +15,12 @@ class CJX:
             self.setup_parser = self.subparsers.add_parser('setup', help='Setting up environment for JavaFX development')
             self.setup_parser.add_argument('sdk_path', help='Path of the JavaFX SDK')
             self.doctor_parser = self.subparsers.add_parser('doctor', help='checks if the necessary pre-requisites are installed')
+            self.path_parser = self.subparsers.add_parser('set-path', help='sets the path of the CJX CLI')
+            self.init_parser = self.subparsers.add_parser('init', help='initializes the CJX CLI')
             self.args = None
             self.project_name = None
+            self.cjx_path = None
+
 
     def run(self):
         try:
@@ -30,33 +32,68 @@ class CJX:
     def parse_args(self):
         self.args = self.parser.parse_args()
 
+    def init(self):
+        try:
+            if not os.path.exists('c:/.cjx'):
+                os.chdir('c:/')
+                os.mkdir('.cjx')
+                os.chdir('.cjx')
+                with open('utils_cjx.json', 'w') as f:
+                    json.dump({}, f, indent=4)
+
+                with open('utils_cjx.json', 'r') as f :
+                    utils_cjx = json.load(f)
+
+                utils_cjx["cjxPath"] = ""
+
+                with open('utils_cjx.json', 'w') as f:
+                    json.dump(utils_cjx, f, indent=4)
+
+                print("CJX CLI initialized successfully 🎉")                
+            else:
+                print("Error: CJX CLI already initialized")
+        except Exception as e:
+            print(f'Error: {e}')
+        
+
     def handle_command(self):
         command = self.args.command
-        if command == 'create':
-            self.handle_create_command()
-        elif command == 'setup':
-            self.handle_setup_command()
-        elif command == 'doctor':
-            self.handle_doctor_command()
+        if command == 'init':
+            self.init()
+        elif command in ['create', 'setup', 'doctor', 'set-path']:
+            if os.path.exists('c:/.cjx'):
+                self.cjx_path = 'c:/.cjx/utils_cjx.json'
+                if command == 'create':
+                    self.handle_create_command()
+                elif command == 'setup':
+                    self.handle_setup_command()
+                elif command == 'doctor':
+                    self.handle_doctor_command()
+                elif command == 'set-path':
+                    self.set_path()
+            else:
+                print("Error: CJX CLI not initialized")
         else:
-            raise Exception(f'Invalid command: {command}')
+            print(f'Error: Invalid command: {command}')
 
     def handle_create_command(self):
         if self.args.project_type == 'simple':
             self.project_name = self.args.project_name
-            print(f'Creating simple JavaFX project {self.project_name}...')
+            print(f'\n\tCreating simple JavaFX project {self.project_name}...')
             self.create_directory()
         else:
-            raise Exception('Invalid project type')
+            print('Error: Invalid project type')
 
     def handle_setup_command(self):
         if not os.path.exists(self.args.sdk_path):
-            raise Exception('JavaFX SDK not found')
-        print('JavaFX SDK found')
-        self.set_sdk_path(self.args.sdk_path)
+            print('Error: JavaFX SDK not found')
+        else:
+            print('JavaFX SDK found')
+            self.set_sdk_path(self.args.sdk_path)
 
     def handle_doctor_command(self):
         check1 = "Checking if Java is installed: "
+        result1 = ""
         command = "java -version"
         try:
             subprocess.check_output(command, stderr=subprocess.STDOUT, shell=True)
@@ -65,40 +102,103 @@ class CJX:
             result1 = "\033[1mJava is not installed ❌\033[0m"
         print("{}{}".format(check1, result1))
 
+        check3 = "Checking if CJX CLI path is set: "
+        result3 = ""
+        
+        with open(self.cjx_path, 'r') as f:
+            path = json.load(f)
+
+        if path['cjxPath'] != "":
+            result3 = "\033[1mCJX CLI path is set ✔️\033[0m"
+        else:
+            result3 = "\033[1mCJX CLI path is not set ❌\033[0m"
+
+
+        print("{}{}".format(check3, result3))
+
         check2 = "Checking if JavaFX is setup: "
+        result2 = ""
         try:
-            with open('bin/constants.json', 'r') as f:
-                constants = json.load(f)
-            if os.path.exists(constants['javafxPath']):
-                result2 = "\033[1mJavaFX is setup ✔️\033[0m"
-            else:
-                result2 = "\033[1mJavaFX is not setup ❌\033[0m"
+
+            with open(self.cjx_path, 'r') as f:
+                path = json.load(f)
+
+            utils_path_json = f"{path['cjxPath']}/utils/utils_path.json"
+
+            try:
+                with open(utils_path_json, 'r') as f:
+                    utils_path = json.load(f)
+                if os.path.exists(utils_path['javafxPath']):
+                    result2 = "\033[1mJavaFX is setup ✔️\033[0m"
+                else:
+                    result2 = "\033[1mJavaFX is not setup ❌\033[0m"
+            except:
+                result2 = "\033[1mJavaFX is not setup, because CJX CLI path is not set ❌\033[0m"
+
         except:
-            print('Error checking JavaFX setup')
+            result2 = "\033[1mError checking JavaFX Setup\033[0m"
         
         print("{}{}".format(check2, result2))
 
+
+    def set_path(self):
+        if os.path.exists('cjx.exe') or os.path.exists('cjx.py'):
+            current_dir = os.getcwd()   
+            try:
+                with open('c:/.cjx/utils_cjx.json', 'r') as f:
+                    path = json.load(f)
+
+                current_dir = current_dir.replace('\\', '/')
+                path['cjxPath'] = current_dir
+
+                with open('c:/.cjx/utils_cjx.json', 'w') as f:
+                    json.dump(path, f, indent=4)
+
+                print('CJX CLI path set successfully to', current_dir)
+            except:
+                print('Error setting CJX path, check your current path. It has to be in the same directory as the dependencies folder.')
+        else:
+            print('Error: CJX executable not found, check your current path. It has to be in the same directory as the cjx executable.')
+
     def set_sdk_path(self, sdk_path):
         try:
-            with open('bin/constants.json', 'r') as f:
-                constants = json.load(f)
-            constants['javafxPath'] = sdk_path
-            constants['jarPath'] = sdk_path + '/lib'
-            with open('bin/constants.json', 'w') as f:
-                json.dump(constants, f, indent=4)
+            with open(self.cjx_path, 'r') as f:
+                path = json.load(f)
+
+            if path['cjxPath'] == "":
+                print('Error: CJX CLI path not set')
+            else:
+                utils_path_json = f"{path['cjxPath']}/utils/utils_path.json"
+
+                with open(utils_path_json, 'r') as f:
+                    utils_path = json.load(f)
+                utils_path['javafxPath'] = sdk_path
+                utils_path['jarPath'] = sdk_path + '/lib'
+                with open(utils_path_json, 'w') as f:
+                    json.dump(utils_path, f, indent=4)
+
+                print('JavaFX SDK path set successfully to', sdk_path)
         except:
-            raise Exception('Error setting JavaFX SDK path')
+            print('Error setting JavaFX SDK path')
+
+    def get_cjx_path(self):
+        with open(self.cjx_path, 'r') as f:
+            path = json.load(f)
+        return path['cjxPath']  
         
     def create_directory(self):
         try:
-            new_folder = os.path.join(self.current_dir, f'{self.project_name}')
+            new_folder = self.project_name
             if not os.path.exists(new_folder):
-                print(f'Creating directory {self.project_name}')
+                print(f'\tCreating directory {self.project_name}\n')
                 os.mkdir(new_folder)
                 os.chdir(new_folder)
                 subprocess.run(['git', 'init'])
                 self.create_gitignore()
                 self.create_readme()
+
+                print(f'\n\t\033[1mProject {self.project_name} created successfully 🎇\033[0m\n')
+                print(f"To open the project in VS Code, type '\033[1mcd {self.project_name}\033[0m', then type '\033[1mcode .\033[0m'\n")
             else:
                 print('Project already exists')
         except OSError as e:
@@ -106,11 +206,15 @@ class CJX:
         
     def create_gitignore(self):
         try:
-            os.chdir(self.current_dir)
-            with open('src/simple/.gitignore', 'r') as f:
-                git_ignore = f.read()
-            os.chdir(self.project_name)
             print('Creating .gitignore file')
+            git_ignore = '''            
+# Ignore Visual Studio Code directory
+.vscode/
+
+# Ignore compiled binary files
+bin/
+            '''
+
             with open('.gitignore', 'w') as f:
                 f.write(git_ignore)
             self.vscode_folder()
@@ -118,15 +222,13 @@ class CJX:
             print(f"Error creating .gitignore file: {e}")
 
     def create_readme(self):
+        os.chdir('..')
         try:
-            os.chdir(self.current_dir)
             print('Creating README.md file')
-            with open('src/simple/README.md') as f:
+            with open(f'{self.get_cjx_path()}/src/simple/README.md') as f:
                 readme = f.read()
             
             readme = readme.replace('project_name', f"Project Name: {self.project_name}")
-
-            os.chdir(self.project_name)
 
             with open('README.md', 'w') as f:
                 f.write(readme)
@@ -135,9 +237,6 @@ class CJX:
 
     def vscode_folder(self):
         try:
-            # create .vscode folder
-            os.chdir(self.current_dir)
-            os.chdir(self.project_name)
             os.mkdir('.vscode')
             self.create_launch_json()
             self.create_settings_json()
@@ -147,10 +246,10 @@ class CJX:
     def create_launch_json(self):
         try:
             print('Creating launch.json file')
-            with open('../src/simple/.vscode/launch.json', 'r') as f:
+            with open(f'{self.get_cjx_path()}/src/simple/.vscode/launch.json', 'r') as f:
                 launch = json.load(f)
 
-            with open('../bin/constants.json', 'r') as f:
+            with open(f'{self.get_cjx_path()}/utils/utils_path.json', 'r') as f:
                 constants = json.load(f)
 
             module_path = constants['javafxPath'] + '/lib'
@@ -160,21 +259,21 @@ class CJX:
 
             with open('.vscode/launch.json', 'w') as f:
                 json.dump(launch, f, indent=4)
+
         except (OSError, json.JSONDecodeError) as e:
             print(f"Error creating launch.json file: {e}")
 
     def create_settings_json(self):
         try:
             print('Creating settings.json file')
-            with open('../src/simple/.vscode/settings.json', 'r') as f:
+            with open(f'{self.get_cjx_path()}/src/simple/.vscode/settings.json', 'r') as f:
                 settings = json.load(f)
 
-            with open('../bin/constants.json', 'r') as f:
+            with open(f'{self.get_cjx_path()}/utils/utils_path.json', 'r') as f:
                 constants = json.load(f)
 
             jar_path = constants['jarPath']
 
-            # files = os.listdir(jar_path)
 
             settings_filled = settings["java.project.referencedLibraries"] = []
             settings_filled.append("lib/**/*.jar")
@@ -212,8 +311,7 @@ class CJX:
     def create_src_files(self):
         try:
             print('Creating src files')
-            # os.chdir(self.current_dir)
-            with open('../../src/simple/src/App.java.txt', 'r') as f:
+            with open(f'{self.get_cjx_path()}/src/simple/src/App.java.txt', 'r') as f:
                 app = f.read()
 
             with open('App.java', 'w') as f:
