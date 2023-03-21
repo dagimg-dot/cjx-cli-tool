e09479b6393952402bf1521e22e8df2382788a76
Dagim G. Astatkie <workflow.jd@gmail.com>
2023-03-21 15:26:20 +0300

    refactoring code for better readability

--- cjx.py
+++ cjx.py
@@ -2,11 +2,11 @@ import argparse
 import json
 import os
 import subprocess
-
+from simple import Simple
 
 class CJX:    
     def __init__(self):
-            self.parser = argparse.ArgumentParser(prog='cjx')
+            self.parser = argparse.ArgumentParser(prog='cjx', description='CJX CLI', usage='%(prog)s [command] [options]')
             self.subparsers = self.parser.add_subparsers(dest='command')
             self.doctor_parser = self.subparsers.add_parser('doctor', help='checks if the necessary pre-requisites are installed')
             self.init_parser = self.subparsers.add_parser('init', help='initializes the CJX CLI')
@@ -20,7 +20,6 @@ class CJX:
             self.args = None
             self.project_name = None
             self.cjx_path = None
-
 
     def run(self):
         try:
@@ -81,22 +80,22 @@ class CJX:
                 elif command == 'doctor':
                     self.handle_doctor_command()
                 elif command == 'set-path':
-                    self.set_path()
+                    self.set_cjx_path()
             else:
                 print("Error: CJX CLI not initialized")
         elif command is None:
             self.cjx_logo()
             self.parser.print_help()
         else:
-            print(f'Error: Invalid command: {command}')
-                                                          
+            print(f'Error: Invalid command: {command}')  
+
     def handle_create_command(self):
         if self.args.project_type == 'simple':
             self.project_name = self.args.project_name
-            print(f'\n\tCreating simple JavaFX project {self.project_name}...')
-            self.create_directory()
+            print(f'\n\tCreating simple JavaFX project {self.project_name}\n')
+            Simple.handle_simple(self)
         else:
-            print('Error: Invalid project type')
+            print('Error: Invalid project type')                                                   
 
     def handle_setup_command(self):
         if not os.path.exists(self.args.sdk_path):
@@ -163,8 +162,7 @@ class CJX:
         
         print("{}{}".format(check3, result3))
 
-
-    def set_path(self):
+    def set_cjx_path(self):
         if os.path.exists('cjx.exe') or os.path.exists('cjx.py'):
             current_dir = os.getcwd()   
             try:
@@ -208,140 +206,6 @@ class CJX:
         with open(self.cjx_path, 'r') as f:
             path = json.load(f)
         return path['cjxPath']  
-        
-    def create_directory(self):
-        try:
-            new_folder = self.project_name
-            if not os.path.exists(new_folder):
-                print(f'\tCreating directory {self.project_name}\n')
-                os.mkdir(new_folder)
-                os.chdir(new_folder)
-                subprocess.run(['git', 'init'])
-                self.create_gitignore()
-                self.create_readme()
-
-                print(f'\n\t\033[1mProject {self.project_name} created successfully 🎇\033[0m\n')
-                print(f"To open the project in VS Code, type '\033[1mcd {self.project_name}\033[0m', then type '\033[1mcode .\033[0m'\n")
-            else:
-                print('Project already exists')
-        except OSError as e:
-            print(f"Error creating directory: {e}")
-        
-    def create_gitignore(self):
-        try:
-            print('Creating .gitignore file')
-            git_ignore = '''            
-# Ignore Visual Studio Code directory
-.vscode/
-
-# Ignore compiled binary files
-bin/
-            '''
-
-            with open('.gitignore', 'w') as f:
-                f.write(git_ignore)
-            self.vscode_folder()
-        except OSError as e:
-            print(f"Error creating .gitignore file: {e}")
-
-    def create_readme(self):
-        os.chdir('..')
-        try:
-            print('Creating README.md file')
-            with open(f'{self.get_cjx_path()}/src/simple/README.md') as f:
-                readme = f.read()
-            
-            readme = readme.replace('project_name', f"Project Name: {self.project_name}")
-
-            with open('README.md', 'w') as f:
-                f.write(readme)
-        except OSError as e:
-            print(f"Error creating README.md file: {e}")
-
-    def vscode_folder(self):
-        try:
-            os.mkdir('.vscode')
-            self.create_launch_json()
-            self.create_settings_json()
-        except OSError as e:
-            print(f"Error creating .vscode folder: {e}")
-
-    def create_launch_json(self):
-        try:
-            print('Creating launch.json file')
-            with open(f'{self.get_cjx_path()}/src/simple/.vscode/launch.json', 'r') as f:
-                launch = json.load(f)
-
-            with open(f'{self.get_cjx_path()}/utils/utils_path.json', 'r') as f:
-                constants = json.load(f)
-
-            module_path = constants['javafxPath'] + '/lib'
-
-            for config in launch['configurations']:
-                config['vmArgs'] = f"--module-path \"{module_path}\" --add-modules javafx.controls,javafx.fxml"
-
-            with open('.vscode/launch.json', 'w') as f:
-                json.dump(launch, f, indent=4)
-
-        except (OSError, json.JSONDecodeError) as e:
-            print(f"Error creating launch.json file: {e}")
-
-    def create_settings_json(self):
-        try:
-            print('Creating settings.json file')
-            with open(f'{self.get_cjx_path()}/src/simple/.vscode/settings.json', 'r') as f:
-                settings = json.load(f)
-
-            with open(f'{self.get_cjx_path()}/utils/utils_path.json', 'r') as f:
-                constants = json.load(f)
-
-            jar_path = constants['jarPath']
-
-
-            settings_filled = settings["java.project.referencedLibraries"] = []
-            settings_filled.append("lib/**/*.jar")
-
-            for jarfiles in constants['jarFiles']:
-                if not jarfiles.endswith('.jar'):
-                    continue
-                settings_filled.append(f"{jar_path}/{jarfiles}")
-
-            with open('.vscode/settings.json', 'w') as f:
-                json.dump(settings, f, indent=4)
-
-            self.create_bin_folder()
-            self.create_src_folder()
-        except Exception as e:
-            print(f'Error creating settings.json file: {e}')
-
-    def create_bin_folder(self):
-        try:
-            print('Creating bin folder')
-            os.mkdir('bin')
-        except OSError as e:
-            print(f"Error creating bin folder: {e}")
-
-    def create_src_folder(self):
-        try:
-            print('Creating src folder')
-            os.mkdir('src')
-            os.chdir('src')
-            self.create_src_files()
-        except OSError as e:
-            print(f"Error creating src folder: {e}")
-
-
-    def create_src_files(self):
-        try:
-            print('Creating src files')
-            with open(f'{self.get_cjx_path()}/src/simple/src/App.java.txt', 'r') as f:
-                app = f.read()
-
-            with open('App.java', 'w') as f:
-                f.write(app)
-        except OSError as e:
-            print(f"Error creating src files: {e}")
-
 
 if __name__ == '__main__':
     CJX().run()
